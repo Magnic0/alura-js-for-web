33b5e3104a410cbed03fe351e8288d65515dcc74
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-06 20:34:20 -0300

    JS: keyboard now interacts with the app

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ const soundList = document.querySelectorAll('.som');
 
 // Condição que varre ambas as listas e relaciona uma com a outra
 for(let i = 0; i < keyList.length; i++) {
-    keyList[i].onclick = () => {
-        soundList[i].play();
+    
+    let key = keyList[i];
+    let sound = soundList[i];
+
+    key.onclick = () => {
+        sound.play();
     };
+
+    key.onkeydown = () => {
+        key.classList.add('ativa');
+    }
+
+    key.onkeyup = () => {
+        key.classList.remove('ativa');
+    }
+    
 }
