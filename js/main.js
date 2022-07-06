0d75bf0155b3fb90ff84c1e6995df6e94d9fcd4a
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-05 22:14:17 -0300

    JS: assign all sound buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 document.querySelector('.tecla_pom').onclick = () => {
         document.querySelector('#som_tecla_pom').play();
 };
+
+document.querySelector('.tecla_clap').onclick = () => {
+        document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_tim').onclick = () => {
+    document.querySelector('#som_tecla_tim').play();
+};
+
+document.querySelector('.tecla_puff').onclick = () => {
+    document.querySelector('#som_tecla_puff').play();
+};
+
+document.querySelector('.tecla_splash').onclick = () => {
+    document.querySelector('#som_tecla_splash').play();
+};
+
+document.querySelector('.tecla_toim').onclick = () => {
+    document.querySelector('#som_tecla_toim').play();
+};
+
+document.querySelector('.tecla_psh').onclick = () => {
+    document.querySelector('#som_tecla_psh').play();
+};
+
+document.querySelector('.tecla_tic').onclick = () => {
+    document.querySelector('#som_tecla_tic').play();
+};
+
+document.querySelector('.tecla_tom').onclick = () => {
+    document.querySelector('#som_tecla_tom').play();
+};
