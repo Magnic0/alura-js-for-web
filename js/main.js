b5171755921ec900998c6dd0b165c672150d3a2b
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-05 23:52:38 -0300

    JS: simplify buttons logic

--- js/main.js
+++ js/main.js
@@ -1,35 +1,11 @@
-document.querySelector('.tecla_pom').onclick = () => {
-        document.querySelector('#som_tecla_pom').play();
-};
+// Lista de botões
+const keyList = document.querySelectorAll('.tecla');
+// Lista de sons
+const soundList = document.querySelectorAll('.som');
 
-document.querySelector('.tecla_clap').onclick = () => {
-        document.querySelector('#som_tecla_clap').play();
+// Condição que varre ambas as listas e relaciona uma com a outra
+for(let i = 0; i < keyList.length; i++) {
+    keyList[i].onclick = () => {
+        soundList[i].play();
+    };
 }
-
-document.querySelector('.tecla_tim').onclick = () => {
-    document.querySelector('#som_tecla_tim').play();
-};
-
-document.querySelector('.tecla_puff').onclick = () => {
-    document.querySelector('#som_tecla_puff').play();
-};
-
-document.querySelector('.tecla_splash').onclick = () => {
-    document.querySelector('#som_tecla_splash').play();
-};
-
-document.querySelector('.tecla_toim').onclick = () => {
-    document.querySelector('#som_tecla_toim').play();
-};
-
-document.querySelector('.tecla_psh').onclick = () => {
-    document.querySelector('#som_tecla_psh').play();
-};
-
-document.querySelector('.tecla_tic').onclick = () => {
-    document.querySelector('#som_tecla_tic').play();
-};
-
-document.querySelector('.tecla_tom').onclick = () => {
-    document.querySelector('#som_tecla_tom').play();
-};
