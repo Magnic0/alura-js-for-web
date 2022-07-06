535923c3341796908d2eaf252adf2164d7acaf20
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-05 22:04:16 -0300

    JS: fix pom

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-function pom() {
-    document.querySelector('#som_tecla_pom').play();
-}
+document.querySelector('.tecla_pom').onclick = () => {
+        document.querySelector('#som_tecla_pom').play();
+};
