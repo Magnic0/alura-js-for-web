f1825d12222b3e2cd8268fcb02a7c587898eed03
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-06 21:56:35 -0300

    JS: bind numpad keys to each MIDI key

--- js/main.js
+++ js/main.js
@@ -3,20 +3,42 @@ const keyList = document.querySelectorAll('.tecla');
 // Lista de sons
 const soundList = document.querySelectorAll('.som');
 
-// Condição que varre ambas as listas e relaciona uma com a outra
+function playSound(num) { // num representa uma tecla do MIDI
+    keyList[num].focus();
+    keyList[num].classList.add('ativa');
+    soundList[num].play();
+}
+
 for(let i = 0; i < keyList.length; i++) {
     
     let key = keyList[i];
     let sound = soundList[i];
 
-    key.onclick = () => {
+    key.focus();
+
+    key.onclick = (event) => {
         sound.play();
     };
 
     key.onkeydown = (event) => {
-        if(event.code === "Space") {
-            key.classList.add('ativa');
-            sound.play();
+        if(event.code === "Numpad7") {
+            playSound(0);
+        } else if(event.code === "Numpad8") {
+            playSound(1);
+        } else if(event.code === "Numpad9") {
+            playSound(2);
+        } else if(event.code === "Numpad4") {
+            playSound(3);
+        } else if(event.code === "Numpad5") {
+            playSound(4);
+        } else if(event.code === "Numpad6") {
+            playSound(5);
+        } else if(event.code === "Numpad1") {
+            playSound(6);
+        } else if(event.code === "Numpad2") {
+            playSound(7);
+        } else if(event.code === "Numpad3") {
+            playSound(8);
         }
     }
 
